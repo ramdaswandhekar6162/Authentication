10eccbbdcb73326c2078cabd4c42e40dc3bfe6ff
ramdaswandhekar6162 <98158100+ramdaswandhekar6162@users.noreply.github.com>
2023-12-27 22:14:30 +0530

    remove cipher Authentication and add md5 hash Authentications

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
+//const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -25,7 +26,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt,{secret : process.env.SECRET, encryptedFields:["password"]});
+//userSchema.plugin(encrypt,{secret : process.env.SECRET, encryptedFields:["password"]});
 
 const User = new mongoose.model("User",userSchema);
 
@@ -45,7 +46,7 @@ app.post("/register",function(req,res){
 
     const newUser = new User({
         email : req.body.username,
-        password : req.body.password
+        password : md5(req.body.password)
     });
     
     newUser.save(newUser).then(function(result){
@@ -60,7 +61,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const email = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email : email}).then(function(result){
         if(result){
